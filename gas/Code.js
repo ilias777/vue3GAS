e594567b233180392ca6caf57be2bf3ce4053da2
Ilias <dougias.ilias@gmail.com>
2023-11-20 11:58:28 +0100

    Add doGet function in Code.js file

--- gas/Code.js
+++ gas/Code.js
@@ -1,3 +1,10 @@
-function myFunction() {
-  
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+function doGet(e) {
+  return HtmlService.createTemplateFromFile('index.html')
+    .evaluate()
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1.0')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+    .setTitle('Vue3 GAS')
 }
